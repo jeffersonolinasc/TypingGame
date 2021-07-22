5f40e0b0eae7f3ab94bf735596d028b995880bde
Jefferson Nascimento <jefferson.oliv@hotmail.com>
2021-07-22 20:22:58 -0300

    Projeto Inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Words from './components/Words';
+import Container from './components/Container';
+import Typeracer from './components/Typeracer';
+import Results from './components/Results';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [word, setWord] = useState(Words);
+  const [newWord, setNewWord] = useState(word[0]);
+  const [disabled, setDisabled] = useState(false);
+  const [correctResults, setCorrectResults] = useState([]);
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCorrect, setCountCorrect] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState('');
+  const [animation, setAnimation] = useState(null);
+
+
+  const checkAnswer = () => {
+    if (inputValue.trim() === newWord) {
+      setCorrectResults(prevCorrect => [...prevCorrect, newWord]);
+      setCountCorrect(prevCorrect => prevCorrect + 1);
+      return;
+    }
+    setWrongResults(prevWrong => [...prevWrong, prevWrong]);
+  }
+
+  const handleInput = e => {
+    if (e.charCode === 13 && inputValue.trim() !== '') {
+      checkAnswer();
+      setNewWord(word[randomWord]);
+      setInputValue('');
+    }
+  }
+
+  const handleStart = () => {
+    setDisabled(!disabled);
+    setCorrectResults([]);
+    setWrongResults([]);
+    setCountCorrect(0);
+    setInputValue("");
+  }
+
+  let randomWord = Math.floor(Math.random() * word.length);
+
+  useEffect(() => {
+    if (time <= 30 && time !== 0 && disabled === false) {
+      setTimeout(() => setTime(prevTime => prevTime - 1), 1000);
+    }
+    else if (disabled) {
+      setTime(30);
+      setAnimation(null);
+    }
+    else if (time === 0) {
+      setDisabled(true);
+    }
+
+
+    if (time <= 10) {
+      setAnimation("scaleNumber 2s infinite");
+    }
+  }, [disabled, time]);
+
+
+  useEffect(() => {
+    setNewWord(word[randomWord]);
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Typeracer
+          newWord={newWord}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          disabled={disabled}
+          time={time}
+          animation={animation}
+          handleInput={handleInput}
+          handleStart={handleStart}
+        />
+      </Container>
+
+
+      <Results
+        correctResults={correctResults}
+        wrongResults={wrongResults}
+        countCorrect={countCorrect}
+      />
     </div>
   );
 }
---
+++ src/components/Typeracer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from './Button';
+
+const Typeracer = ({ newWord, inputValue, setInputValue, disabled, time, animation, handleInput, handleStart }) => {
+
+
+
+    return (
+        <div className="typeRacer">
+            <div className="wordOutput">
+                <p>{newWord}</p>
+            </div>
+
+            <div className="time">
+                <p style={{ animation: animation !== null ? animation : "" }}>{time}</p>
+            </div>
+
+            <div className="wordValues">
+                <input
+                    type="text"
+                    disabled={disabled && disabled}
+                    onKeyPress={e => handleInput(e)}
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    placeholder={disabled ? '' : 'Start typing....'}
+                />
+                <Button handleStart={handleStart} disabled={disabled} />
+
+            </div>
+        </div>
+    );
+}
+
+export default Typeracer;
